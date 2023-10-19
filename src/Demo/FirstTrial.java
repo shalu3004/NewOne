2e98691d67d8d695333650fa83c42b6f9354c39c
Shalinu Saju <shalinu.saju@codinghands.in>
2023-10-19 10:56:42 +0530

    entire project

---
+++ src/Demo/FirstTrial.java
@@ -0,0 +1,16 @@
+package Demo;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstTrial {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\codin\\eclipse-workspace\\NewOne\\chromedriver.exe");
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://www.amazon.in");
+        driver.manage().window().maximize();
+	}
+
+}
+
+
